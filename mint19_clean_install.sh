259aa3cacb851054eb628f155939e7a992f057de
Anton Bagliy <taccessviolation@gmail.com>
2018-10-10 20:04:03 +0300

    ADD: jetbrains, sublime, docker

--- mint19_clean_install.sh
+++ mint19_clean_install.sh
@@ -24,4 +24,40 @@ dpkg -i dropbox64.deb
 # wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 # ~/.dropbox-dist/dropboxd
 
+apt-get install -y snap
+apt-get install -y snapd
+# snap install phpstorm --classic
+# snap install intellij-idea-community --classic --edge
+
+# might as well install everything when needed through toolbox manually
+wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.11.4269.tar.gz
+tar -xvzf jetbrains-toolbox-1.11.4269.tar.gz
+rm jetbrains-toolbox-1.11.4269.tar.gz
+
+#sublime-text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+apt-get update
+apt-get install -y sublime-text
+
+# docker
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   bionic \
+   stable"
+
+apt-get update
+apt-get install -y docker-ce
+
+# TODO: mono, docker?
+
 
