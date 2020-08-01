cf119c8e8405914e0283989f6e69d0c252c9802f
Anton Bagliy <taccessviolation@gmail.com>
2020-08-01 17:48:39 +0300

    FIX: comment

--- code_server_install.sh
+++ code_server_install.sh
@@ -7,6 +7,6 @@ tar -xzvf code-server-3.2.0-linux-x86_64.tar.gz
 sudo cp -r code-server-3.2.0-linux-x86_64 /usr/lib/code-server
 sudo ln -s /usr/lib/code-server/code-server /usr/bin/code-server
 sudo mkdir /var/lib/code-server
-sudo nano /lib/systemd/system/code-server.service
+#sudo nano /lib/systemd/system/code-server.service
 sudo systemctl enable code-server
 sudo systemctl start code-server
