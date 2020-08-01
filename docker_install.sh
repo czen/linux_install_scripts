26630c94c2125c5f0a6139169020f298f4e1d20f
Anton Bagliy <taccessviolation@gmail.com>
2020-08-01 18:15:25 +0300

    ADD: docker&compose install script

---
+++ docker_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https  ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(grep "UBUNTU_CODENAME" /etc/os-release | awk '{split($1,a,"="); print a[2]}') stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
+sudo usermod -aG docker czen
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
