88fade30edf9ea3904ead2ecb7a23ef6d63c57f4
Anton Bagly <taccessviolation@gmail.com>
2020-12-20 00:03:44 +0300

    UPD: code-server v3.8.0

--- code_server_install.sh
+++ code_server_install.sh
@@ -2,9 +2,9 @@
 sudo cp code-server.service /lib/systemd/system/code-server.service
 mkdir ~/code-server
 cd ~/code-server
-wget https://github.com/cdr/code-server/releases/download/3.2.0/code-server-3.2.0-linux-x86_64.tar.gz
-tar -xzvf code-server-3.2.0-linux-x86_64.tar.gz
-sudo cp -r code-server-3.2.0-linux-x86_64 /usr/lib/code-server
+wget https://github.com/cdr/code-server/releases/download/v3.8.0/code-server-3.8.0-linux-amd64.tar.gz
+tar -xzvf code-server-3.8.0-linux-amd64.tar.gz
+sudo cp -r code-server-3.8.0-linux-amd64 /usr/lib/code-server
 sudo ln -s /usr/lib/code-server/code-server /usr/bin/code-server
 sudo mkdir /var/lib/code-server
 #sudo nano /lib/systemd/system/code-server.service
