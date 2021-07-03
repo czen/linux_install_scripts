19e439c99e0768364cf2570ecd812ac67569cbe0
Anton Bagly <taccessviolation@gmail.com>
2021-07-04 02:12:34 +0300

    ADD: install virtualbox for ubutu focal

---
+++ virtualbox.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#sudo apt-get install -y software–properties–common
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add –
+wget –q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian focal contrib"
+sudo apt-get update
+sudo apt-get install virtualbox-6.1
+#sudo apt-get install virtualbox—ext–pack
