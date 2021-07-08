561d0d28e73c1f27b4895cd5210bc1a5ecaea365
Anton Bagly <taccessviolation@gmail.com>
2021-07-08 17:54:15 +0300

    ADD: kvm initial setup commands

---
+++ kvm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install qemu-kvm virtinst bridge-utils cpu-checker
+#kvm-ok
+sudo cp /etc/network/interfaces /etc/network/interfaces.bakup-1
+# sudo vi /etc/network/interfaces
+#sudo systemctl restart networking
+#sudo brctl show
+sudo apt-get install virt-manager
