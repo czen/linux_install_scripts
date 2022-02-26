36b4ffd2a94a869e8554e40fac088e90db74ae72
czen <taccessviolation@gmail.com>
2022-02-27 00:36:43 +0300

    Update video.sh

--- video.sh
+++ video.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get install -y blender shotcut kdenlive openshot
-sudo apt install xorriso libssl1.1 ocl-icd-opencl-dev fakeroot
+sudo apt install -y xorriso libssl1.1 ocl-icd-opencl-dev fakeroot
 # https://www.fosslinux.com/24381/how-to-install-davinci-resolve-on-ubuntu.htm
