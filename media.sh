a0f0615737dda99233795d13ad235a2388b6bdb9
czen <taccessviolation@gmail.com>
2022-03-04 03:44:12 +0300

    ADD: more packages

--- media.sh
+++ media.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo apt-get install -y gimp krita pinta inkscape digikam rawtherapee darktable
+sudo apt-get install -y obs-studio obs-plugins
 #photivo
 #lightzone
--- utils.sh
+++ utils.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-sudo apt-get install -y build-essential locate
-sudo apt-get install -y git
+sudo apt-get install -y build-essential locate patool
+sudo apt-get install -y git git-gui git-all
 sudo apt-get install -y graphviz filezilla keepassx flameshot copyq
 sudo apt-get install -y konsole fish meld screen htop mc nfs-common mcedit kate vlc smartmontools krusader geany ffmpeg
 sudo apt-get install -y smlnj okular
 sudo apt-get install -y ruby-full rustc golang scala qt5-default cmake qtcreator
 sudo apt-get install -y p7zip p7zip-full
 sudo apt-get install -y w3m lynx links2
+sudo apt-get install -y qbittorrent
+sudo apt-get install -y bats cde coop-computing-tools coop-computing-tools-dev coop-computing-tools-doc
+sudo apt-get install -y cde impressive
+#sudo apt-get install -y htcondor htcondor-dev
+sudo apt-get install -y copyq copyq-plugins freeipmi gcalcli
